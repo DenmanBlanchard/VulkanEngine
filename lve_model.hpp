ef1f517fd4fa1132e2d36f13c7cd6ec53771c077
Denmam Blanchard <Denman.Blanchard1@outlook.com>
2025-12-08 20:25:38 -0500

    First Commit

---
+++ lve_model.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+#include "lve_device.hpp"
+#include <vulkan/vulkan_core.h>
+namespace lve {
+    class LveModel {
+        public:
+
+        struct Vertex {
+            glm::vec2 position;
+            glm::vec3 color;
+
+            static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+            static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+        };
+
+        LveModel(LveDevice &device, const std::vector<Vertex> &verticies);
+        ~LveModel();
+
+        LveModel(const LveModel &) = delete;
+        LveModel &operator=(const LveModel &) = delete;
+
+        void bind(VkCommandBuffer commandBuffer);
+        void draw(VkCommandBuffer commandBuffer);
+
+        private:
+        void createVertexBuffers(const std::vector<Vertex> &verticies);
+
+        LveDevice& lveDevice;
+        VkBuffer vertexBuffer;
+        VkDeviceMemory vertexBufferMemory;
+        uint32_t vertexCount;
+    };
+}
---
+++ lve_window.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <vulkan/vulkan_core.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+namespace lve {
+    class LveWindow{
+
+        public:
+        LveWindow(int w, int h, std::string name);
+        ~LveWindow();
+
+        LveWindow(const LveWindow &) = delete;
+        LveWindow &operator=(const LveWindow &) = delete;
+
+        bool shouldClose() {return glfwWindowShouldClose(window);}
+        VkExtent2D getExtent() { return {static_cast<uint32_t>(width), static_cast<uint32_t>(height)}; }
+        bool wasWindowResized() {return frameBufferResized;}
+        void resetWindowResizedFlag() {frameBufferResized = false;}
+
+        void createWindowSurface(VkInstance instance, VkSurfaceKHR *surface);
+
+        GLFWwindow *window;
+
+        private:
+        static void framebufferResizeCalllback(GLFWwindow *window, int width, int height);
+        void initWindow();
+
+        int width;
+        int height;
+        bool frameBufferResized = false;
+
+        std::string windowName;
+    };
+}
