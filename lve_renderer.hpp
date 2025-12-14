40e9808c0801b7ad36ee39ff9c494974da2f9c98
Denman Blanchard <Denman.Blanchard1@outlook.com>
2025-12-13 21:10:31 -0500

    Made it work with windows

--- lve_renderer.hpp
+++ lve_renderer.hpp
@@ -10,47 +10,49 @@
 #include <vector>
 #include <vulkan/vulkan_core.h>
 
-namespace lve {
-class LveRenderer {
- public:
+namespace lve
+{
+  class LveRenderer
+  {
+  public:
+    LveRenderer(LveWindow &window, LveDevice &device);
+    ~LveRenderer();
 
-  LveRenderer(LveWindow &window, LveDevice &device);
-  ~LveRenderer();
+    LveRenderer(const LveRenderer &) = delete;
+    LveRenderer &operator=(const LveRenderer &) = delete;
 
-  LveRenderer(const LveRenderer &) = delete;
-  LveRenderer &operator=(const LveRenderer &) = delete;
+    VkRenderPass getSwapChainRenderPass() const { return lveSwapChain->getRenderPass(); }
+    bool isFrameInProgress() const { return isFrameStarted; }
 
-  VkRenderPass getSwapChainRenderPass() const {return lveSwapChain->getRenderPass();}
-  bool isFrameInProgress() const {return isFrameStarted;}
+    VkCommandBuffer getCurrentCommandBuffer() const
+    {
+      assert(isFrameStarted && "Cannot get commandbuffer when frame not in progress");
+      return commandBuffers[currentFrameIndex];
+    }
 
-  VkCommandBuffer getCurrentCommandBuffer() const {
-    assert(isFrameStarted && "Cannot get commandbuffer when frame not in progress");
-    return commandBuffers[currentFrameIndex];
-  }
+    int getFrameIndex() const
+    {
+      assert(isFrameStarted && "Cannot get frame index when fraem is not in progress");
+      return currentFrameIndex;
+    }
 
-  int getFrameIndex() const {
-    assert(isFrameStarted && "Cannot get frame index when fraem is not in progress");
-    return currentFrameIndex;
-  }
+    VkCommandBuffer beginFrame();
+    void endFrame();
+    void beginSwapChainRenderPass(VkCommandBuffer commandBuffer);
+    void endSwapChainRenderPass(VkCommandBuffer commmandBuffer);
 
-  VkCommandBuffer beginFrame();
-  void endFrame();
-  void beginSwapChainRenderPass(VkCommandBuffer commandBuffer);
-  void endSwapChainRenderPass(VkCommandBuffer commmandBuffer);
+  private:
+    void createCommandBuffers();
+    void freeCommandBuffers();
+    void recreateSwapChain();
 
-
- private:
-  void createCommandBuffers();
-  void freeCommandBuffers();
-  void recreateSwapChain();
-
-  LveWindow& lveWindow;
-  LveDevice& lveDevice;
-  std::unique_ptr<LveSwapChain> lveSwapChain;
-  std::vector<VkCommandBuffer> commandBuffers;
-
-  uint32_t currentImageIndex;
-  int currentFrameIndex;
-  bool isFrameStarted;
-};
-}  // namespace lve
+    LveWindow &lveWindow;
+    LveDevice &lveDevice;
+    std::unique_ptr<LveSwapChain> lveSwapChain;
+    std::vector<VkCommandBuffer> commandBuffers;
+    // track frame/image indices and state; initialize to safe defaults to avoid UB
+    uint32_t currentImageIndex = 0;
+    int currentFrameIndex = 0;
+    bool isFrameStarted = false;
+  };
+} // namespace lve
