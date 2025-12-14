433170e4663949ea4e3ebb6e6c4cc1f460c7ae32
Denmam Blanchard <Denman.Blanchard1@outlook.com>
2025-12-14 16:36:44 -0500

    Finished tutorial 14

--- lve_camera.hpp
+++ lve_camera.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <glm/ext/matrix_float4x4.hpp>
+#include <glm/ext/vector_float3.hpp>
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
@@ -12,9 +13,15 @@ namespace lve {
 
     void setPerspectiveProjection(float fovy, float aspect, float near, float far);
 
+    void setViewDirection(glm::vec3 position, glm::vec3 direction, glm::vec3 up = glm::vec3{0.f, -1.f, 0.f});
+    void setViewTarget(glm::vec3 position, glm::vec3 target, glm::vec3 up = glm::vec3{0.f, -1.f, 0.f});
+    void setViewYXZ(glm::vec3 position, glm::vec3 rotation);
+
     const glm::mat4& getProjection() const {return projectionMatrix;}
+    const glm::mat4& getView() const {return viewMatrix;}
 
     private:
     glm::mat4 projectionMatrix{1.f};
+    glm::mat4 viewMatrix{1.f};
   };
 }
